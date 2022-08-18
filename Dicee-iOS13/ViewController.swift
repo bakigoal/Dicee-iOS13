8088484a4f5c07a7cbcfcd22df806eee34549bfa
Бакиров Ильмир Ильгизович (4066171) <bakirov@dev.vtb.ru>
2022-08-18 16:13:53 +0500

    increase/ decrease dice number

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -6,14 +6,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     let images = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+    var leftDiceNumber = 5
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = images[5]
-        diceImageView2.image = images[4]
+        diceImageView1.image = images[leftDiceNumber]
+        diceImageView2.image = images[rightDiceNumber]
+        
+        leftDiceNumber = (leftDiceNumber + 1) % images.count
+        rightDiceNumber = (rightDiceNumber + 5) % images.count
     }
     
 }
